4d0b515e18717542f597e17b9c3f7d4c46cbedba
Dag Ågren <dag.agren@wolt.com>
2024-11-04 15:32:39 +0200

    Add comment about strange escaping behaviour.

--- Source/MainActorPublisher/MainActorFuture.swift
+++ Source/MainActorPublisher/MainActorFuture.swift
@@ -6,6 +6,10 @@ public struct MainActorFuture<Output, Failure: Error>: MainActorPublisher {
     private let future: Future<Output, Failure>
 
     @MainActor public init(_ attemptToFulfill: @MainActor (@escaping Promise) -> Void) {
+        // Apparently the callback in Future.init will be called immediately from the
+        // init function and not escape, even though it is marked as @escaping.
+        // Here we assume this is happening and don't mark our version as @escaping,
+        // and use `withoutActuallyEscaping()` to work around the incorrect labelling.
         self.future = withoutActuallyEscaping(attemptToFulfill) { attemptToFulfill in
             Future<Output, Failure> { promise in
                 attemptToFulfill {
